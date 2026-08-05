0558b9cf0c4909104c1661e4348f700b830dde85
Darsh <darshbhatt089@gmail.com>
2026-08-05 10:24:43 +0530

    feat : Home Slice added

---
+++ src/redux/features/homeSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+    photos: [],
+    videos: [],
+    gifs: [],
+    loading: false,
+    error: null,
+}
+
+const homeSlice = createSlice({
+
+    name: 'home',
+
+    initialState,
+
+    reducers: {
+
+        setLoading(){
+            state.loading = true;
+            state.error = null;
+        },
+
+        setHomeData(state, action) {
+            
+            state.photos = action.payload.photos;
+            state.videos = action.payload.videos;
+            state.gifs = action.payload.gifs;
+            state.loading = false
+        },
+
+        setError(state, action) {
+
+            state.loading = false;
+            state.error = action.payload;
+        }
+
+    },
+})
+
+export const {
+    setError,
+    setHomeData,
+    setLoading
+} = homeSlice.actions;
+
+export default homeSlice.reducer;
